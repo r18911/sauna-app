148fe13070c7687ad6cbfa7efa10cdd70942c793
r18911 <ryousuke18911@icloud.com>
2025-03-13 14:49:16 +0900

    rooms作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,4 +11,5 @@ Rails.application.routes.draw do
   # root "posts#index"
   root to: 'saunas#index'
   resources :saunas
+  resources :rooms
 end
