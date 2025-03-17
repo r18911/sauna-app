5da9a5c1fa63ff7d8263d48dc9ca761aa814a859
r18911 <ryousuke18911@icloud.com>
2025-03-17 10:45:03 +0900

    マイグレーションファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_13_090741) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_17_013704) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_13_090741) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "payments", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "reservation_id", null: false
+    t.integer "amount", null: false
+    t.integer "payment_status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reservation_id"], name: "index_payments_on_reservation_id"
   end
 
   create_table "reservations", charset: "utf8mb3", force: :cascade do |t|
@@ -89,6 +98,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_13_090741) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "payments", "reservations"
   add_foreign_key "reservations", "rooms"
   add_foreign_key "reservations", "users"
 end
