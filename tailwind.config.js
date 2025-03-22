579c12c3ac5557f33a0eb56152749ef910da98f0
r18911 <ryousuke18911@icloud.com>
2025-03-22 23:57:51 +0900

    tailwind.css削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/views/**/*.{html,erb}',
+    './app/assets/stylesheets/**/*.{scss,css}',
+    './app/javascript/**/*.js'
+  ],
   theme: {
     extend: {},
   },
