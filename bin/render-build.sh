e6b0f5acb7c3d3e42b28b664d2a21e67b5650c86
r18911 <ryousuke18911@icloud.com>
2025-03-22 21:31:35 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,22 @@
 # exit on error
 set -o errexit
 
+# -------------------------
+# Node.js & Yarnのインストール追加
+# -------------------------
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+apt-get install -y nodejs
+npm install -g yarn
+
+# -------------------------
+# 既存のビルド処理
+# -------------------------
+bundle install
+yarn install --check-files
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
