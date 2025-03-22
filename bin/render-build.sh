a2209c1d1510a7c9f9e220d1593d74147c1f46a7
r18911 <ryousuke18911@icloud.com>
2025-03-23 01:00:35 +0900

    scss→cssに変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,19 +2,9 @@
 # exit on error
 set -o errexit
 
-# -------------------------
-# Node.js & Yarnのインストール追加
-# -------------------------
-# curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
-# apt-get install -y nodejs
-# npm install -g yarn
-
-# -------------------------
-# 既存のビルド処理
-# -------------------------
 yarn install --check-files
 yarn build:css
-
 bundle install
 bundle exec rake assets:precompile
+bundle exec rake assets:clean
 bundle exec rake db:migrate
