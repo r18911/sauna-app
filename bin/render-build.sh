e8012073ef3048c00c4eeaf5f18a922473b50da6
r18911 <ryousuke18911@icloud.com>
2025-03-23 01:06:35 +0900

    不要ファイル削除

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
-yarn install --check-files
-yarn build:css
+# yarn install --check-files
+# yarn build:css
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
