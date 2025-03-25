12ccbf44a034f2407f87ac8f99e63401bb67cb4f
r18911 <ryousuke18911@icloud.com>
2025-03-25 10:43:24 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
   # Deviseのログイン・新規登録後のリダイレクト先を変更
   def after_sign_in_path_for(resource)
     rooms_path # ログイン後は部屋一覧ページへ
@@ -8,12 +13,16 @@ class ApplicationController < ActionController::Base
     rooms_path # 新規登録後も部屋一覧ページへ
   end
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  end
+
 end
